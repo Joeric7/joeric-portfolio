ee710e52aa9ea92b12dd5ea73da633e21f8868f2
Joeric7 <joericmena@gmail.com>
2022-09-08 16:59:53 -0400

    add animations to slider

--- scripts/script.js
+++ scripts/script.js
@@ -8,17 +8,20 @@ let activeImg = imgList[counter];
 
 
 const slideForward = () => {
+    imgList[0].style.animation = '';
     imgContainer.appendChild(imgList[0]);
-    imgList = document.querySelectorAll('.img-container img')
+    imgList = document.querySelectorAll('.img-container img');
+    imgList[0].style.animation = 'forward 1s';
 }
-
 
 const slideBackward = () => {
+    imgList[0].style.animation = '';
     imgContainer.insertBefore(imgList[imgList.length - 1], imgList[0]);
     imgList = document.querySelectorAll('.img-container img');
+    imgList[0].style.animation = 'backward 1s';
 }
 
-// setInterval(slideForward, 5000);
+setInterval(slideForward, 2000);
 
 arrowBack.addEventListener('click', slideBackward);
 arrowForward.addEventListener('click', slideForward);
